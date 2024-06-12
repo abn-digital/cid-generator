46f5eedb74b32f69e2f64ec6f039e2f2645b4f92
Lancha <68738783+joacoc2020@users.noreply.github.com>
2024-06-12 15:55:01 -0300

    Changed base on Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
+  // https://vitejs.dev/guide/static-deploy.html#github-pages
+  base: "/cid-generator/",
   plugins: [svelte(), viteSingleFile()]
 });
